23562c60301c35bf0b31a0d9a11eb2b221e99d77
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2022-11-07 19:43:36 -0600

    add first script

---
+++ first.py
@@ -0,0 +1,16 @@
+# python code
+# script_name: nose.py
+# author:jromerooo2
+# description: Cool Music
+
+from earsketch import *
+init()
+setTempo(120)
+# How to use fitMedia:
+# fitMedia (SOUND_CLIP_HERE, LAYER/track , start_measure, stop_measure)
+fitMedia (HIPHOP_BASSSUB_001, 2, 1, 10)
+fitMedia (HIPHOP_FUNKBEAT_002, 3, 1, 10)
+fitMedia (CIARA_SET_VOX_HOOK_1, 4, 1, 10)
+fitMedia (YG_NEW_HIP_HOP_PIANO_5, 5, 1, 10)
+fitMedia (Y11_DRUM_SAMPLES_1, 6,1,10)
+finish()
